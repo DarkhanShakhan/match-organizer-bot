7ed77147268032c0fbe1114674ca4f61a3223c79
complexusprada <complexusprada@gmail.com>
2023-06-25 13:36:26 +0600

    added payment methods: not functional yet

--- main.go
+++ main.go
@@ -1,27 +1,37 @@
 package main
 
-import (
-	// "fmt"
+// "fmt"
 
-	// "github.com/complexusprada/kaspi_perevod/kaspi"
-)
+// "github.com/complexusprada/kaspi_perevod/kaspi"
 
 func main() {
-	// kaspi := kaspi.NewKaspiDevice()
-	// ticket, err := kaspi.SignIn("phone", "password")
+	// device := kaspi.NewKaspiDevice()
+	// ticket, err := device.SignIn("username", "password")
 	// if err != nil {
 	// 	fmt.Println(err)
- //    return
+	// 	return
 	// }
 	//
-	// payments, err := kaspi.Payments("24.06.2023", "24.06.2023", ticket)
-	// if err != nil {
+ //  
+	// transaction := kaspi.NewTransaction(ticket, "7475849384", 100)
+ //  err  = transaction.Make()
+ //  if err != nil {
 	// 	fmt.Println(err)
  //    return
+ //  }
+	//
+ //  fmt.Println("success ")
+
+	// fmt.Println(fio)
+
+	// payments, err := kaspi.Payments("23.06.2023", "25.06.2023", ticket)
+	// if err != nil {
+	// 	fmt.Println(err)
+	//    return
 	// }
 	//
- //  matchPayments := kaspi.MatchPayments("178", payments)
- //  participant := kaspi.MatchParticipantPayment("178", "2873", payments)
+	//  matchPayments := kaspi.MatchPayments("178", payments)
+	//  participant := kaspi.MatchParticipantPayment("178", "2873", payments)
 	//
 	//
 	// fmt.Println(matchPayments)
